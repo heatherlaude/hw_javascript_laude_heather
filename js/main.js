b4a5e538eebf4d7543b4403ab205bb594be56a6c
Heather Laude <hlaude@ufl.edu>
2015-01-31 14:32:38 -0500

    added variables for last five presidents

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var obama = {
+    firstName: "Barack",
+    lastName: "Obama",
+    termLength: 2,
+    party: "Democrat",
+    yearsOfPresidency: "2009-2016"
+};
+
+var georgeWBush = {
+    firstName: "George W",
+    lastName: "Bush",
+    termLength: 2,
+    party: "Republican",
+    yearsOfPresidency: "2001-2009"
+};
+
+var clinton = {
+    firstName: "Bill",
+    lastName: "Clinton",
+    termLength: 2,
+    party: "Democrat",
+    yearsOfPresidency: "1993-2001"
+};
+
+var georgeHWBush = {
+    firstName: "George HW",
+    lastName: "Bush",
+    termLength: 1,
+    party: "Republican",
+    yearsOfPresidency: "1989-1993"
+};
+
+var reagan = {
+    firstName: "Ronald",
+    lastName: "Reagan",
+    termLength: 2,
+    party: "Republican",
+    yearsOfPresidency: "1981-1989"
+};
