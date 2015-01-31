507008f84cefc6ff265d7c90bd2e82c6c6051f37
Heather Laude <hlaude@ufl.edu>
2015-01-31 15:59:04 -0500

    added numba function expression

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function(string) {
+    if (string.length < 7) {
+        console.log("What a short little word!")
+    }
+    
+    if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count.")
+    }
+    
+    if (string.length == 7) {
+        console.log("7, what a perfect choice!")
+    }
+    
+};
